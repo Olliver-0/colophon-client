2e1fa730db87e03c3c5d1facbfa431fc49b2f205
Olliver-0 <nata.gabriel.de.oliveira@gmail.com>
2025-08-03 19:14:02 -0300

    feat(routing): implement protected and public route system
    This commit introduces a routing system that differentiates between
    public and protected routes based on the user's authentication state.
    
    A reusable `ProtectedRoute` component was created to act as a gatekeeper,
    redirecting unauthenticated users to the login page. This ensures that
    sensitive application areas are only accessible after a successful login.
    
    - Implemented the `<ProtectedRoute />` wrapper component.
    - Updated `AppRoutes.tsx` to wrap protected routes like `/dashboard` and
      `/bookshelves` with the new component.
    - Unauthorized access attempts to protected routes now redirect to `/login`.

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,13 +2,19 @@ import { Route, Routes } from 'react-router-dom';
 import { SignUpPage } from '@/pages/SignUpPage/SignUpPage';
 import { LoginPage } from '@/pages/LoginPage/LoginPage';
 import { HomePage } from '@/pages/HomePage/HomePage';
+import { ProtectedRoute } from './ProtectedRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/register" element={<SignUpPage />} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route element={<PublicRoute />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
+
+      <Route element={<ProtectedRoute />}></Route>
     </Routes>
   );
 };
